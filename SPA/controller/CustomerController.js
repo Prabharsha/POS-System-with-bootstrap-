48170ca1450615a14db8b09bebc9c8b573cc158d
Prabharsha <pansiluprabharsha1@gamil.com>
2022-03-14 01:59:16 +0530

    updated

--- SPA/controller/CustomerController.js
+++ SPA/controller/CustomerController.js
@@ -1,3 +1,5 @@
+generateCustomerId();
+
 function saveCustomer() {
     let customerID = $("#customerIDInput").val();
     let customerName = $("#customerNameInput").val();
@@ -15,7 +17,7 @@ function saveCustomer() {
     swal({
         position: 'top',
         icon: 'success',
-        title: 'Your work has been saved',
+        title: 'Customer Registered Successful.!',
         showConfirmButton: false,
         timer: 2000
     })
@@ -43,8 +45,35 @@ function updateCustomer() {
     var customerDTO = new CustomerDTO(customerID, customerName, customerAddress, customerCity, customerPostalCode);
     customerArray.push(customerDTO);
     loadCustomerTable1();
-    alert("updated")
+    swal({
+        position: 'top',
+        icon: 'info',
+        title: 'Changes Updated',
+        showConfirmButton: false,
+        timer: 1500
+    })
 
+}
+
+function deleteCustomer() {
+
+    $("#customerTableReg").empty();
+    for (let i = 0; i < customerArray; i++) {
+        console.log("inside for")
+        if ($("#customerIDInput").val() == customerArray[i].getCustomerID()) {
+            customerArray.splice(i, 1);
+            clearFields();
+        }
+    }
+
+    loadCustomerTable1();
+    swal({
+        position: 'top',
+        icon: 'success',
+        title: 'Customer Deleted.!',
+        showConfirmButton: false,
+        timer: 1500
+    })
 }
 
 function loadCustomerTable1() {
@@ -64,6 +93,8 @@ function loadCustomerTable1() {
     /*tbl listener / double click set data to fields*/
     $("#customerTableReg>tr").dblclick(function () {
 
+        setBtn();
+
         let tempCusID = $(this).children(':nth-child(1)').text();
         $("#customerIDInput").val(tempCusID);
 
@@ -82,7 +113,165 @@ function loadCustomerTable1() {
     });
 
 }
+function setBtn(){
+    $("#btnSaveCustomer").attr('disabled',true);
+    $("#btnUpdateCustomer").attr('disabled',false);
+    $("#btnCustomerDelete").attr('disabled',false);
+}
 
+function generateCustomerId() {
+
+    if (customerArray.length !== 0) {
+        var cusId = customerArray[customerArray.length - 1].getCustomerID();
+        let splitTxt = cusId.split("C", 2);
+        let newCusId = parseInt(splitTxt[1]) + 1;
+
+        if (parseInt(newCusId) <= 9) {
+            $("#customerIDInput").val("C00" + newCusId);
+
+        } else if (parseInt(newCusId) <= 99) {
+            $("#customerIDInput").val("C0" + newCusId);
+
+        } else if (parseInt(newCusId) <= 99) {
+            $("#customerIDInput").val("C" + newCusId);
+        }
+    } else {
+        $("#customerIDInput").val("C001").css('font-weight', 'bold');
+
+    }
+}
+
+/*validations*/
+var regExCusName = /^([A-z\s]{3,20})$/;
+var regExCusAddress = /^([A-z0-9/,\s]{3,})$/;
+var regExCity = /^([A-z]{3,20})$/;
+var regExPostalCode = /^([0-9]{3,5})$/;
+
+/*name*/
+function validateCustomerName(){
+    let input =$("#customerNameInput").val();
+
+    if(regExCusName.test(input)){
+        $("#errorName").text("");
+        $("#customerNameInput").css('border','2px solid green');
+
+        $("#customerNameInput").keydown(function (e){
+           if(e.key == 'Enter'){
+               $("#customerAddressInput").focus();
+           }
+        });
+        return true;
+    }else {
+        $("#customerNameInput").css('border','2px solid red');
+        $("#errorName").text("Wrong Format !");
+        return false;
+    }
+}
+
+$("#customerNameInput").keyup(function (){
+   disableRegistrationBtn();
+});
+
+/*address*/
+function validateCustomerAddress(){
+    let input =$("#customerAddressInput").val();
+
+    if(regExCusAddress.test(input)){
+        $("#errorAddress").text("");
+        $("#customerAddressInput").css('border','2px solid green');
+
+        $("#customerAddressInput").keydown(function (e){
+            if (e.key == 'Enter'){
+                $("#customerCityInput").focus();
+            }
+        });
+        return true;
+    }else {
+        $("#customerAddressInput").css('border','2px solid red');
+        $("#errorAddress").text("Wrong Format !");
+        return false;
+
+    }
+}
+$("#customerAddressInput").keyup(function (){
+    disableRegistrationBtn();
+});
+
+/*city*/
+function validateCustomerCity(){
+    let input =$("#customerCityInput").val();
+
+    if(regExCity.test(input)){
+        $("#errorCity").text("");
+        $("#customerCityInput").css('border','2px solid green');
+
+        $("#customerCityInput").keydown(function (e){
+            if(e.key == 'Enter'){
+                $("#customerPostalCodeInput").focus();
+            }
+        });
+        return true;
+    }else {
+        $("#customerCityInput").css('border','2px solid red');
+        $("#errorCity").text("Wrong Format !");
+        return false;
+    }
+}
+
+$("#customerCityInput").keyup(function (){
+    disableRegistrationBtn();
+});
+
+/*postal code*/
+function validatePostalCode(){
+    let input =$("#customerPostalCodeInput").val();
+
+    if(regExPostalCode.test(input)){
+        $("#errorPostal").text("");
+        $("#customerPostalCodeInput").css('border','2px solid green');
+
+        return true;
+    }else {
+        $("#customerPostalCodeInput").css('border','2px solid red');
+        $("#errorPostal").text("Wrong Format !");
+        return false;
+
+    }
+}
+$("#customerPostalCodeInput").keyup(function (){
+    disableRegistrationBtn();
+});
+
+
+/*button disabling*/
+function disableRegistrationBtn() {
+    if (validateAll()) {
+        $("#btnSaveCustomer").attr('disabled', false);
+    } else {
+        $("#btnSaveCustomer").attr('disabled', true);
+    }
+}
+
+function validateAll() {
+    if (validateCustomerName()) {
+        if (validateCustomerAddress()) {
+            if (validateCustomerCity()) {
+                if (validatePostalCode()) {
+                        return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+}
 
 function clearFields() {
     $("#customerIDInput").val("");
@@ -92,10 +281,19 @@ function clearFields() {
     $("#customerPostalCodeInput").val("");
 }
 
+
+
 $("#btnUpdateCustomer").click(function () {
     updateCustomer();
+    generateCustomerId();
 });
 
 $("#btnSaveCustomer").click(function () {
     saveCustomer();
+    generateCustomerId();
+});
+
+$("#btnCustomerDelete").click(function () {
+    deleteCustomer();
+    generateCustomerId();
 });
